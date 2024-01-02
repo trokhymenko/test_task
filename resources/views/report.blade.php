036b6ebf9b94ffa68ffd38105d728a687332021f
trokhymenko <jura.salg@gmail.com>
2024-01-03 01:08:36 +0200

    Add view template

--- resources/views/report.blade.php
+++ resources/views/report.blade.php
@@ -1,10 +1,9 @@
 @extends('layouts.default')
-
 @section('content')
     <div class="container">
         <div class="row">
             <div class="offset-3 col-5">
-                <form action="{{route('home')}}" method="get" class="mt-4 mb-2">
+                <form action="{{route('report')}}" method="get" class="mt-4 mb-2">
                     <div class="input-group">
                         <input type="number" name="search" id="search" value="{{$search}}" class="form-control rounded-start" placeholder="Search Driver ID" />
                         <button type="submit" class="btn btn-outline-primary">search</button>
@@ -12,27 +11,23 @@
                 </form>
             </div>
             <div class="col-3 mt-4 mb-2">
-                <a href="{{route('report')}}" class="btn btn-primary">Calculate Payable Time</a>
+                <a href="{{route('home')}}" class="btn btn-primary">Back</a>
             </div>
         </div>
         <div class="row">
-            <div class="col-12">
+            <div class="offset-3 col-6">
                 <table class="table">
                     <thead>
                     <tr>
-                        <th scope="col">Trip ID</th>
                         <th scope="col">Driver ID</th>
-                        <th scope="col">Pickup</th>
-                        <th scope="col">Dropoff</th>
+                        <th scope="col">Payable Time (minutes)</th>
                     </tr>
                     </thead>
                     <tbody>
-                    @foreach($trips as $trip)
+                    @foreach($drivers as $driver)
                         <tr>
-                            <td>{{$trip->trip_id}}</td>
-                            <td>{{$trip->driver_id}}</td>
-                            <td>{{$trip->pickup}}</td>
-                            <td>{{$trip->dropoff}}</td>
+                            <td>{{$driver->driver_id}}</td>
+                            <td>{{$driver->time}}</td>
                         </tr>
                     @endforeach
                     </tbody>
@@ -41,7 +36,7 @@
         </div>
         <div class="row">
             <div class="col-12">
-                {{$trips->links()}}
+                {{$drivers->links()}}
             </div>
         </div>
     </div>
